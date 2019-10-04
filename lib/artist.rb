f4949fef13b5bfde7ab84fd5c3fcec264fd45ca5
Alfredo Diaz <mail4alfredo@gmail.com>
2019-10-04 14:57:33 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,14 @@
+class Artist
+    
+  
+  # def initialize(name, artist, genre)
+  #   @name=name 
+  #   @artist=artist 
+  #   @genre=genre
+  # end
+  
+ 	# def name=(new_name)
+	 # @name = new_name
+  # end
+  
+end  
---
+++ lib/song.rb
@@ -0,0 +1,47 @@
+class Song 
+  attr_accessor :name 
+  attr_reader :artist, :genre 
+  @@all=[]
+  
+  def initialize(name, artist=nil, genre=nil)
+    @name=name 
+    #@artist=artist 
+    
+    self.artist= artist if artist 
+    
+    # @genre=genre 
+    self.genre= genre if genre
+  end   
+  
+  def self.all
+    @@all 
+  end  
+  
+  def self.destroy_all
+    all.clear
+  end  
+  
+  def save
+    self.class.all << self
+  end
+  
+  def self.create(name)
+    song=new(name)
+    song.save
+    song
+  end   
+  
+  def artist=(artist)
+    @artist=artist
+    artist.add.song(self)
+  end  
+  
+  def genre=(genre)
+    @genre=genre
+    genre.songs << self if !genre.songs.include?(self)
+  end  
+
+
+
+  
+end  
